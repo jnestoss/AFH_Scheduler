cea195dcfedc289b233b828af763825eae44c806
Josh Nestoss <jnestoss@careerpathservices.org>
2019-01-12 21:01:04 -0800

    Merged Gabriel Branch with changes made to history tab from previous repo.

--- AFH-Scheduler/AFH_Scheduler/History/HistoryVM.cs
+++ AFH-Scheduler/AFH_Scheduler/History/HistoryVM.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using AFH_Scheduler;
 using AFH_Scheduler.Algorithm;
+using AFH_Scheduler.Complete;
 using AFH_Scheduler.Data;
 using AFH_Scheduler.Database;
 using AFH_Scheduler.Helper_Classes;
@@ -31,6 +32,82 @@ namespace AFH_Scheduler.History
             }
         }
 
+        public HistoryVM()
+        {
+            _homes = new ObservableCollection<HistoryModel>();
+            _isexpanded = false;
+            GenData();
+        }
+        private RelayCommand _openEditHistoryDialog;
+        public ICommand OpenEditHistoryDialogCommand
+        {
+            get
+            {
+                if (_openEditHistoryDialog == null)
+                    _openEditHistoryDialog = new RelayCommand(EditHistoryDialogOpen);
+                return _openEditHistoryDialog;
+            }
+        }
+        private RelayCommand _loadhomehistory;
+        public ICommand LoadHomeHistoryCommand
+        {
+            get
+            {
+                if (_loadhomehistory == null)
+                    _loadhomehistory = new RelayCommand(LoadHomeHistory);
+                return _loadhomehistory;
+            }
+        }
+        private OpenMessageDialogService _messageService;
+        public OpenMessageDialogService MessageService
+        {
+            get
+            {
+                if (_messageService == null)
+                    _messageService = new OpenMessageDialogs();
+                return _messageService;
+            }
+        }
+        private string _followUpDate;
+        public string FollowUpDate
+        {
+            get { return _followUpDate; }
+            set
+            {
+                _followUpDate = value;
+                OnPropertyChanged("FollowUpDate");
+            }
+        }
+        private bool _isexpanded;
+        public bool IsExpanded
+        {
+            get { return _isexpanded; }
+            set
+            {
+                _isexpanded = value;
+                OnPropertyChanged("IsExpanded");
+            }
+        }
+        private void LoadHomeHistory(object obj)
+        {
+            IsExpanded = true;
+        }
+        private void EditHistoryDialogOpen(object obj)
+        {
+            RescheduleVM rescheduleVM = new RescheduleVM(FollowUpDate, MessageService);
+            var updateOrNot = MessageService.ShowDialog(rescheduleVM);
+            if (updateOrNot == true)
+            {
+                FollowUpDate = rescheduleVM.RescheduledFollowUpDate;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return "History";
+            }
+        }
         private RelayCommand _refreshTable;
         public ICommand RefreshTableCommand
         {
@@ -47,21 +124,6 @@ namespace AFH_Scheduler.History
             Homes.Clear();
             GenData();
         }
-
-        public HistoryVM()
-        {
-            _homes = new ObservableCollection<HistoryModel>();
-
-            GenData();
-        }
-
-
-        public string Name {
-            get {
-                return "History";
-            }
-        }
-
         public void GenData()
         {
             using (HomeInspectionEntities db = new HomeInspectionEntities())
