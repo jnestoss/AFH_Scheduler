af757e1154930fe46af552e52259082a8cfdfa5c
Josh Nestoss <jnestoss@careerpathservices.org>
2019-02-04 18:05:23 -0800

    Added LoginDB, User class, and UserFactory class

---
+++ AFH-Scheduler/AFH_Scheduler/Database/LoginDB/User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFH_Scheduler.Database.LoginDB
+{
+    public class User
+    {
+        private string _username;
+        private string _password;
+        private bool _admin;
+
+        public User(String username, String password, bool admin)
+        {
+            _username = username;
+            _password = password;
+            _admin = admin;
+        }
+
+        public String Username
+        {
+            get { return _username; }
+            set
+            {
+                if (value != _username)
+                {
+                    _username = value;
+                }
+            }
+        }
+        public String Password
+        {
+            get { return _password; }
+            set
+            {
+                if (value != _password)
+                {
+                    _password = value;
+                }
+            }
+        }
+        public bool Admin
+        {
+            get { return _admin; }
+            set
+            {
+                if(value != _admin)
+                {
+                    _admin = value;
+                }
+            }
+        }
+    }
+}
---
+++ AFH-Scheduler/AFH_Scheduler/Database/LoginDB/UserFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AFH_Scheduler.Database.LoginDB
+{
+    public static class UserFactory
+    {
+        private static readonly string connection = "metadata=res://*/Database.LoginDB.csdl|res://*/Database.LoginDB.ssdl|res://*/Database.LoginDB.msl;provider=System.Data.SQLite.EF6;provider connection string=&quot;data source=..\\..\\Database\\LoginDB\\LoginDB.db&quot;Password=Afh";
+
+        public static bool CheckPassword(string username, string password)
+        {
+            return true;
+        }
+
+        public static bool CreateUser(string username,string password,bool admin)
+        {
+            return false;
+        }
+
+        public static User[] LoadUsers()
+        {
+            return null;
+        }
+    }
+}
