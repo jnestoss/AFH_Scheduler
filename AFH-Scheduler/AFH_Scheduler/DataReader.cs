61ca514bfc698ec6df26f0e5eb082de564903527
ScottAllen256 <scott.allen256@gmail.com>
2019-03-03 16:35:59 -0800

    readded DataREader

---
+++ AFH-Scheduler/AFH_Scheduler/DataReader.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// methods for reading from database
+namespace AFH_Scheduler
+{
+    class DataReader
+    {
+
+
+
+        
+    }
+}
